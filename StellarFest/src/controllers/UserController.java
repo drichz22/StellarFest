7bbd06ee543fc88a9b9177a78bc56fc5989a206d
drichz22 <aldrich.reinhart@gmail.com>
2024-12-14 00:34:17 +0700

    Update UserController

--- StellarFest/src/controllers/UserController.java
+++ StellarFest/src/controllers/UserController.java
@@ -8,6 +8,13 @@ public class UserController {
 	private LoginView loginView;
 	private RegisterView registerView;
 	
+	public UserController() {
+		this.loginView = new LoginView();
+		this.registerView = new RegisterView();
+	}
 	
+	public void Register(String email, String name, String password, String role) {
+		
+	}
 
 }
