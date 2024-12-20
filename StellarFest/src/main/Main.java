afec0fd15ae0feeebce7bdad640c9edc151d795b
drichz22 <aldrich.reinhart@gmail.com>
2024-12-20 15:33:41 +0700

    Refactor class Main. Hrsny semua [DONE]

--- StellarFest/src/main/Main.java
+++ StellarFest/src/main/Main.java
@@ -1,22 +1,15 @@
 package main;
-import controllers.EventOrganizerController;
-import controllers.UserController;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import views.AcceptGuestInvitationView;
-import views.ChangeProfileView;
-import views.CreateEventView;
-import views.EditEventNameView;
-import views.LoginView;
 import views.RegisterView;
-import views.ViewOrganizedEvents_View;
 
 public class Main extends Application {
 	
 	private static Stage stage;
 	
-	public static void redirect(Scene newScene) {
+	public static void redirect(Scene newScene) { //logika untuk change page
 		stage.setScene(newScene);
 		stage.centerOnScreen();
 		stage.setResizable(false);
@@ -29,24 +22,11 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		stage = primaryStage;
-		RegisterView registerView = new RegisterView();
-//		ViewOrganizedEvents_View organizedEventView = new ViewOrganizedEvents_View();
-//		CreateEventView createEventView = new CreateEventView();
-//		EditEventNameView editEventNameView = new EditEventNameView();
-		//AcceptGuestInvitationView acceptGuestInvitationView = new AcceptGuestInvitationView();
-		
-		//EventOrganizerController eoController = new EventOrganizerController();
-		
-		primaryStage.setTitle("StellarFest");
-
-		primaryStage.setScene(registerView.getRegisterScene());
-		//primaryStage.setScene(organizedEventView.getOrganizedEventScene());
-//		primaryStage.setScene(loginView.getLoginScene());
-//		primaryStage.setScene(organizedEventView.getOrganizedEventScene());
-//		primaryStage.setScene(createEventView.getCreateEventScene());
-//		primaryStage.setScene(editEventNameView.getEditEventNameScene());
-		//primaryStage.setScene(acceptGuestInvitationView.getAcceptInvitationScene());
+		stage = primaryStage; //set stage yang di-redirect ke primary stage
+		RegisterView registerView = new RegisterView(); //Mulai dari Register Page
+	
+		primaryStage.setTitle("StellarFest"); //Nama Program
+	    primaryStage.setScene(registerView.getRegisterScene());
 
 		primaryStage.show();
 	}
